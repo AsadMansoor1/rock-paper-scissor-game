386123e2828930238bdeba8c8baf3e897512d8a6
Asad Mehmood <asadpk80@gmail.com>
2023-03-15 02:28:25 -0400

    Basic Rock, paper and scissor game with functionality implemented

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Dev on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardView()
+        }
+    }
+}
